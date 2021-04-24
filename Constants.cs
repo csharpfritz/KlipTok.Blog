7fa656568f241fd2c0e8c697d3d313f614601307
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2021-04-24 18:12:02 -0400

    Reverted to blog path

--- Constants.cs
+++ Constants.cs
@@ -9,7 +9,7 @@ namespace StatiqBlog
         public const string SiteTitle = "KlipTok Blog - All the latest about KlipTok";
         public const string SiteUri = "https://blog.kliptok.com";
         public const string BlogTitle = "KlipTok";
-        public const string BlogPath = ""; // ex. "blog" indicates https://example.com/blog/
+        public const string BlogPath = "blog"; // ex. "blog" indicates https://example.com/blog/
         public const int PostsPerPage = 10;
     }
 }
